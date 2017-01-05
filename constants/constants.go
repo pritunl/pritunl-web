a5fa27f26cf6e1e1f3131788b72e0d4212d8b0cb
Zachary Huff <zach.huff.386@gmail.com>
2017-01-04 19:23:21 -0500

    Add constants

---
+++ constants/constants.go
@@ -0,0 +1,26 @@
+package constants
+
+import (
+	"os"
+)
+
+var (
+	ReverseProxyHeader = os.Getenv("REVERSE_PROXY_HEADER")
+	RedirectServer     = os.Getenv("REDIRECT_SERVER")
+	BindHost           = os.Getenv("BIND_HOST")
+	BindPort           = os.Getenv("BIND_PORT")
+	InternalHost       = os.Getenv("INTERNAL_ADDRESS")
+	CertPath           = os.Getenv("CERT_PATH")
+	KeyPath            = os.Getenv("KEY_PATH")
+	Ssl                bool
+	Scheme             string
+)
+
+func init() {
+	Ssl = CertPath != "" && KeyPath != ""
+	if Ssl {
+		Scheme = "https"
+	} else {
+		Scheme = "http"
+	}
+}
