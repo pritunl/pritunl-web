1a1b12e8354d5e8d5ef9c8a550c8d62d3c70a1b4
Zachary Huff <zach.huff.386@gmail.com>
2018-12-09 00:16:08 -0500

    Add reverse proxy proto header

--- constants/constants.go
+++ constants/constants.go
@@ -5,15 +5,16 @@ import (
 )
 
 var (
-	ReverseProxyHeader = os.Getenv("REVERSE_PROXY_HEADER")
-	RedirectServer     = os.Getenv("REDIRECT_SERVER")
-	BindHost           = os.Getenv("BIND_HOST")
-	BindPort           = os.Getenv("BIND_PORT")
-	InternalHost       = os.Getenv("INTERNAL_ADDRESS")
-	CertPath           = os.Getenv("CERT_PATH")
-	KeyPath            = os.Getenv("KEY_PATH")
-	Ssl                bool
-	Scheme             string
+	ReverseProxyHeader      = os.Getenv("REVERSE_PROXY_HEADER")
+	ReverseProxyProtoHeader = os.Getenv("REVERSE_PROXY_PROTO_HEADER")
+	RedirectServer          = os.Getenv("REDIRECT_SERVER")
+	BindHost                = os.Getenv("BIND_HOST")
+	BindPort                = os.Getenv("BIND_PORT")
+	InternalHost            = os.Getenv("INTERNAL_ADDRESS")
+	CertPath                = os.Getenv("CERT_PATH")
+	KeyPath                 = os.Getenv("KEY_PATH")
+	Ssl                     bool
+	Scheme                  string
 )
 
 func init() {
