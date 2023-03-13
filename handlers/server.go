0fb2fdb0462138cbda9399e0d5fb4ce391d985c7
Zachary Huff <zach.huff.386@gmail.com>
2023-03-13 05:54:46 -0400

    Add device auth to server handler

--- handlers/server.go
+++ handlers/server.go
@@ -41,6 +41,7 @@ type serverPostPutData struct {
 	Ipv6             bool        `json:"ipv6"`
 	Ipv6Firewall     bool        `json:"ipv6_firewall"`
 	DynamicFirewall  bool        `json:"dynamic_firewall"`
+	DeviceAuth       bool        `json:"device_auth"`
 	BindAddress      string      `json:"bind_address"`
 	Protocol         string      `json:"protocol"`
 	Port             int         `json:"port"`
