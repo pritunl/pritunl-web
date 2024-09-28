13575c9cfff14c7b16dcf97d811dd289b31eccb7
Zachary Huff <zach.huff.386@gmail.com>
2024-09-27 22:21:31 -0400

    Add geo sort to server handler

--- handlers/server.go
+++ handlers/server.go
@@ -41,6 +41,8 @@ type serverPostPutData struct {
 	Ipv6             bool        `json:"ipv6"`
 	Ipv6Firewall     bool        `json:"ipv6_firewall"`
 	DynamicFirewall  bool        `json:"dynamic_firewall"`
+	GeoSort          bool        `json:"geo_sort"`
+	ForceConnect     bool        `json:"force_connect"`
 	DeviceAuth       bool        `json:"device_auth"`
 	BindAddress      string      `json:"bind_address"`
 	Protocol         string      `json:"protocol"`
