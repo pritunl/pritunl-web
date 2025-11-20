b6ad1e2a005ba7709f5398422545748c8dbc3331
Zachary Huff <zach.huff.386@gmail.com>
2025-11-20 05:42:18 -0500

    Update constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,27 +1,15 @@
 package constants
 
-import (
-	"os"
-)
-
 var (
-	ReverseProxyHeader      = os.Getenv("REVERSE_PROXY_HEADER")
-	ReverseProxyProtoHeader = os.Getenv("REVERSE_PROXY_PROTO_HEADER")
-	RedirectServer          = os.Getenv("REDIRECT_SERVER")
-	BindHost                = os.Getenv("BIND_HOST")
-	BindPort                = os.Getenv("BIND_PORT")
-	InternalHost            = os.Getenv("INTERNAL_ADDRESS")
-	SslCert                 = os.Getenv("SSL_CERT")
-	SslKey                  = os.Getenv("SSL_KEY")
+	ReverseProxyHeader      string
+	ReverseProxyProtoHeader string
+	RedirectServer          string
+	BindHost                string
+	BindPort                string
+	InternalHost            string
+	SslCert                 string
+	SslKey                  string
+	WebSecret               *[32]byte
 	Ssl                     bool
 	Scheme                  string
 )
-
-func init() {
-	Ssl = SslCert != "" && SslKey != ""
-	if Ssl {
-		Scheme = "https"
-	} else {
-		Scheme = "http"
-	}
-}
