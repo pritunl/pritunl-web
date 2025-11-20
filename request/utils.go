c1ea83c71943cbec8d97db98ab377ef006ac4610
Zachary Huff <zach.huff.386@gmail.com>
2025-11-20 05:51:11 -0500

    Add abort to request utils

--- request/utils.go
+++ request/utils.go
@@ -1,8 +1,15 @@
 package request
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 	"strings"
+
+	"github.com/dropbox/godropbox/errors"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/render"
+	"github.com/pritunl/pritunl-web/errortypes"
 )
 
 func copyHeader(dst, src *http.Request, key string) {
@@ -25,4 +32,33 @@ func parseRemoteAddr(remoteAddr string) (addr string) {
 	addr = strings.Replace(addr, "[", "", 1)
 	addr = strings.Replace(addr, "]", "", 1)
 	return
+}
+
+func AbortWithStatus(c *gin.Context, code int, msg string) {
+	r := render.String{
+		Format: fmt.Sprintf("%d %s", code, msg),
+	}
+
+	c.Status(code)
+	r.WriteContentType(c.Writer)
+	c.Writer.WriteHeaderNow()
+	r.Render(c.Writer)
+	c.Abort()
+}
+
+func AbortRedirect(c *gin.Context, path string) {
+	u, err := url.Parse(c.Request.URL.String())
+	if err != nil {
+		err = errortypes.RequestError{
+			errors.Wrap(err, "request: URL parse error"),
+		}
+		c.AbortWithError(418, err)
+		return
+	}
+
+	u.Path = path
+	u.RawQuery = ""
+
+	c.Redirect(http.StatusFound, u.String())
+	c.Abort()
 }
